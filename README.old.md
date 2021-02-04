d501b34847e1fa9e2df02424f96b570b57d2b82b
Marko Lukić <lukic@netsearch.cz>
2021-02-04 01:32:06 +0100

    - Init

---
+++ README.old.md
@@ -0,0 +1,4 @@
+# ReactJourney
+Going throught the hoops with ReactJS
+
+# This project is used for testing purposes.
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          React journey starts here.
+        </p>
+        <p>
+          Welcome to my testapp.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
